3ae6a03c237b2c5235d661a5bee6860966c10ddd
bellaappel <bellaappel0@gmail.com>
2021-07-04 13:25:47 -0400

    first commit

---
+++ src/containers/ClassicsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class ClassicsList extends Component {
+    state = {classics: []}
+
+
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
