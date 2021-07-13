1e62d89b5083b35ec2e706e116d5bfe5f8d02673
bellaappel <bellaappel0@gmail.com>
2021-07-13 15:00:29 -0400

    single cocktail page succesfully shows

--- src/components/SingleCocktailPage.js
+++ src/components/SingleCocktailPage.js
@@ -4,12 +4,15 @@ import { useSelector } from 'react-redux'
 export const SingleCocktailPage = ({match}) => {
 
     const cocktailId = match.params.cocktailId
-    const cocktail = useSelector(state => state.cocktails.cocktails.find(cocktail => cocktail.id.toString()=== cocktailId))
+   
+    const cocktail = useSelector(state => state.cocktails.find(cocktail => cocktail.id.toString()=== cocktailId))
         return (
             <div className="card">
+                <h1>{console.log(cocktail)}</h1>
+                <h1>{cocktail.name}</h1>
                 <h1>Cocktail</h1>
-                <h2>{cocktail.name}</h2>
-                <h2>{cocktail.photo}</h2>
+               
+             
                 <h3>Recipes:</h3>
               
             </div>
