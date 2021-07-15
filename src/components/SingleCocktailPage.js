68e2a3af7abd016a99087e419a3d00c423c19a55
bellaappel <bellaappel0@gmail.com>
2021-07-15 17:27:06 -0400

    add description to cocktails

--- src/components/SingleCocktailPage.js
+++ src/components/SingleCocktailPage.js
@@ -11,6 +11,8 @@ export const SingleCocktailPage = ({match}) => {
             <div className="card">
                 <h1>{console.log(cocktail)}</h1>
                 <h1>{cocktail.name}</h1>
+                <h2>{cocktail.desc}</h2>
+                <h4>Recipes: </h4>
                 <h3>Add a Recipe:</h3>
                 <RecipesContainer/>  
             </div>
