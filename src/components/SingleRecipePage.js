2caa3958d3ffad3b43aedb5653d3c090910e534a
bellaappel <bellaappel0@gmail.com>
2021-07-16 12:29:33 -0400

    add singlerecipe page

---
+++ src/components/SingleRecipePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const SingleRecipePage = ({match}) => {
+
+    const recipeId = match.params.recipeId
+   
+    const recipe = useSelector(state => state.recipess.find(recipe => recipe.id.toString()=== recipeId))
+        return (
+            <div className="card">
+                <h1>{console.log(recipe)}</h1>
+                <h1>{recipe.name}</h1>
+                <h2>{recipe.desc}</h2>
+            </div>
+        )
+}
